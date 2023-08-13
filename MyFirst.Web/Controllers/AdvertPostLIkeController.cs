fa8eace7340360938bd9657624389bc849f7bb71
Kristijan Stoimenov <stoimenov.kristijan91@gmail.com>
2023-08-13 18:50:43 +0200

    added like function (console log result if its done on refresh it counts the like per user, conected like to user database)

---
+++ MyFirst.Web/Controllers/AdvertPostLIkeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using MyFirst.Web.Models.Domain;
+using MyFirst.Web.Models.ViewModels;
+using MyFirst.Web.Repositories;
+
+namespace MyFirst.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdvertPostLikeController : ControllerBase
+    {
+        private readonly IAdvertPostLikeRepository advertPostLikeRepository;
+
+        public AdvertPostLikeController(IAdvertPostLikeRepository advertPostLikeRepository)
+        {
+            this.advertPostLikeRepository = advertPostLikeRepository;
+        }
+
+
+
+
+
+
+        [HttpPost]
+        [Route("Add")]
+
+        public async Task<IActionResult> AddLike([FromBody] AddLikeRequest addLikeRequest)
+        {
+            var model = new AdvertPostLike
+            {
+                AdvertPostId = addLikeRequest.AdvertPostId,
+                UserId = addLikeRequest.UserId,
+            };
+
+            await advertPostLikeRepository.AddLikeforAdvert(model);
+
+            return Ok();
+
+        }
+    }
+}
